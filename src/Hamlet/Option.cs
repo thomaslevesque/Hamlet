694579ae7ed57cd7b52a1cb36af0df8cd2c4a659
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-02-11 00:15:43 +0100

    Add ToNullable and ToOption

--- src/Hamlet/Option.cs
+++ src/Hamlet/Option.cs
@@ -5,5 +5,21 @@ namespace Hamlet
         public static NoneOption None() => default(NoneOption);
         public static Option<T> None<T>() => default(Option<T>);
         public static Option<T> Some<T>(T value) => new Option<T>(value);
+
+        public static T? ToNullable<T>(this Option<T> option)
+            where T : struct
+        {
+            return option.HasValue
+                ? option.Value
+                : default(T?);
+        }
+
+        public static Option<T> ToOption<T>(this T? nullable)
+            where T : struct
+        {
+            return nullable.HasValue
+                ? Some(nullable.Value)
+                : None<T>();
+        }
     }
 }
---
+++ tests/Hamlet.Tests/OptionTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Hamlet.Tests
+{
+    public class OptionTests
+    {
+        [Fact]
+        public void ToNullable_returns_null_for_none()
+        {
+            var option = Option.None<int>();
+            option.ToNullable().Should().BeNull();
+        }
+
+        [Fact]
+        public void ToNullable_returns_value_for_some()
+        {
+            var option = Option.Some(42);
+            option.ToNullable().Should().Be(42);
+        }
+
+        [Fact]
+        public void ToOption_returns_none_for_null()
+        {
+            int? nullable = null;
+            nullable.ToOption().Should().BeNone();
+        }
+
+        [Fact]
+        public void ToOption_returns_some_for_non_null()
+        {
+            int? nullable = 42;
+            nullable.ToOption().Should().BeSome(42);
+        }
+    }
+}
